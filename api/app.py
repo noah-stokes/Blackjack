8b82d5747650aeabacafc61771ec7b09fd9a9429
Noah Stokes <noahstokes14@gmail.com>
2025-06-02 22:15:35 -0400

    Move app.py into api/, add vercel.json, and update Flask handler

--- api/app.py
+++ api/app.py
@@ -3,7 +3,11 @@
 from flask import Flask, request, jsonify, render_template
 import random
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    static_folder="../static",
+    template_folder="../templates"
+)
 
 game_state = {
 'deck': [],
